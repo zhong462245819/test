1dc8cff194d353395be736942e9b2f793739b0cf
zy46224581819 <zy49630@163.com>
2019-07-15 20:23:01 +0800

    a

---
+++ src/main/java/com/java/test/test.java
@@ -0,0 +1,14 @@
+package com.java.test;
+
+/**
+ * @ClassName
+ * @Description TODO
+ * @Author zhongyuan
+ * @Date 2019/7/15 20:16
+ * @Version 1.0
+ **/
+public class test {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
